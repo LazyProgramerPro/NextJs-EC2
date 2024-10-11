2f8b22e5175db7253987f6d715831f5bf30bf5a4
[BK] Anh Túc <husthunter97@gmail.com>
2024-10-11 20:32:20 +0700

    initial commit

---
+++ src/db/queries/comments.ts
@@ -0,0 +1,29 @@
+import { Comment } from "@prisma/client";
+import { db } from "@/db";
+import { cache } from "react";
+export type CommentWithAuthor = Comment & {
+  user: {
+    name: string | null;
+    image: string | null;
+  };
+};
+
+// Dùng cache để ghi nhớ các yêu cầu
+
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    return db.comment.findMany({
+      where: {
+        postId,
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+  }
+);
